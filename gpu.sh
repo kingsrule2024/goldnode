8481c59d41eeb5e206144dc6afecfa27d601828a
kingsrule2024 <kingsrule5000@gmail.com>
2025-10-16 06:04:27 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-./miner --pubkey=3jdRadLZyw1FPCMdzM4KKcDXgk3EWeg4ZX9mfD8ZeGPJ8TAwpLHxrwTD97MmQ64KWHAQ4y41Au7T4881N1JzFG3SGRxj3SXgHRFLbHxdKHCYLWW31BaQuzCFecMciyhkMUPB --name=$(hostname) --label=Rental --threads-per-card=2
+./miner --pubkey=3jdRadLZyw1FPCMdzM4KKcDXgk3EWeg4ZX9mfD8ZeGPJ8TAwpLHxrwTD97MmQ64KWHAQ4y41Au7T4881N1JzFG3SGRxj3SXgHRFLbHxdKHCYLWW31BaQuzCFecMciyhkMUPB --name=$(hostname) --label=Rental --threads-per-card=1
 
 # Echo status
 echo "gpu miner is running now'"
