9c54be0d51d015e37534e036dd159ddb671c1f19
kingsrule2024 <kingsrule5000@gmail.com>
2025-11-24 03:47:13 +0530

    Add threads-per-card option to gpu miner command

--- gpu.sh
+++ gpu.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-./miner --pubkey=5yuRCLRTqij1SSFfUutoA3PeqAkB5kcXwuEdGdjXTYN1FQMhMotaYGK --name=$(hostname) --label=Rental 
+./miner --pubkey=5yuRCLRTqij1SSFfUutoA3PeqAkB5kcXwuEdGdjXTYN1FQMhMotaYGK --name=$(hostname) --label=Rental --threads-per-card=3
 
 # Echo status
 echo "gpu miner is running now'"
